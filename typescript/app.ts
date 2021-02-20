1dbe34102f4555e86d776aa1922b4922c367b433
ViniciusGuilhermeOliveira <oviniciusguilherme@gmail.com>
2021-02-20 17:29:18 -0300

    trabalhando com number

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,2 +1,7 @@
-let message:string = "Hello Word!"
+let message:string = "Help me, Obi-Wan Kenobi. You're my only hope !"
 console.log(message)
+
+let episode: number = 4
+console.log ("This episode is "+ episode)
+episode += 1 
+console.log ("the next episode is "+ episode)
--- typescript/dir/app.js
+++ typescript/dir/app.js
@@ -1,2 +1,6 @@
-var message = "Hello Word!";
+var message = "Help me, Obi-Wan Kenobi. You're my only hope !";
 console.log(message);
+var episode = 4;
+console.log("This episode is " + episode);
+episode += 1;
+console.log("the next episode is " + episode);
