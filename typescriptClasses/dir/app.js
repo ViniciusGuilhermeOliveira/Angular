ff0b2d076fcbd96b4d3e7380f4c278f24a459c06
ViniciusGuilhermeOliveira <oviniciusguilherme@gmail.com>
2021-02-20 22:03:40 -0300

    (y)

--- typescriptClasses/dir/app.js
+++ typescriptClasses/dir/app.js
@@ -2,9 +2,13 @@
 exports.__esModule = true;
 var base_ships_1 = require("./base-ships");
 var starfighters_1 = require("./starfighters");
+var _ = require("lodash");
+console.log(_.pad("TypesCript Examples", 40, "="));
 var ship = new base_ships_1.Spacecraft('hyperdrive');
 ship.jumpIntoHyperSpace();
 var falcon = new starfighters_1.MillenniumFalcon();
 falcon.jumpIntoHyperSpace();
 var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
 console.log("Is Falcon good for the job ? " + (goodForTheJob(falcon) ? 'YES' : 'NO'));
+//npm install --save-dev @types/lodash
+//npm install --save lodash@4.14
