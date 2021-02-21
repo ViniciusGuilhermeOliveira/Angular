d96230c37e9865c60ca7345b79bdfb893af1c0c6
ViniciusGuilhermeOliveira <oviniciusguilherme@gmail.com>
2021-02-20 22:03:26 -0300

    uso de lodash, tipos de implementação de functions do JavaScript em TypeScript atraves do npm

--- typescriptClasses/app.ts
+++ typescriptClasses/app.ts
@@ -1,5 +1,8 @@
 import {Spacecraft,Containership} from './base-ships'
 import {MillenniumFalcon} from './starfighters'
+ 
+import * as _ from 'lodash'
+console.log(_.pad("TypesCript Examples",40,"="))
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperSpace()
@@ -9,3 +12,7 @@ falcon.jumpIntoHyperSpace()
 
 let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2 
 console.log(`Is Falcon good for the job ? ${goodForTheJob(falcon)? 'YES': 'NO'}`)
+
+
+//npm install --save-dev @types/lodash
+//npm install --save lodash@4.14
